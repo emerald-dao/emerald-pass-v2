98f3f6b9ca4eb85f3583580938a184083c44dce4
jacob-tucker <jacobtucker818@gmail.com>
2022-09-20 01:20:07 -0500

    faq

--- src/lib/data/faqs.ts
+++ src/lib/data/faqs.ts
@@ -2,27 +2,17 @@ const faqs = [
 	{
 		question: 'What is Emerald City DAO?',
 		answer:
-			'Emerald City is the first DAO on the Flow Blockchain, and also, without any doubt, the coolest DAO in the whole universe.'
+			'Emerald City is the first DAO on the Flow Blockchain. We provide the best education in the space for Cadence and Flow development through Emerald Academy, as well as top-tier projects like FLOAT and Emerald Bot, which are used by nearly every project in the ecosystem.'
 	},
 	{
-		question: 'What is Emerald City DAO?',
+		question: 'If I already have time left and purchase more, will it get added on?',
 		answer:
-			'Emerald City is the first DAO on the Flow Blockchain, and also, without any doubt, the coolest DAO in the whole universe.'
+			'Yep! Any time purchased will be added on to your current timer.'
 	},
 	{
-		question: 'What is Emerald City DAO?',
+		question: 'Will there be more premium features for Emerald Pass holders in the future?',
 		answer:
-			'Emerald City is the first DAO on the Flow Blockchain, and also, without any doubt, the coolest DAO in the whole universe.'
-	},
-	{
-		question: 'What is Emerald City DAO?',
-		answer:
-			'Emerald City is the first DAO on the Flow Blockchain, and also, without any doubt, the coolest DAO in the whole universe.'
-	},
-	{
-		question: 'What is Emerald City DAO?',
-		answer:
-			'Emerald City is the first DAO on the Flow Blockchain, and also, without any doubt, the coolest DAO in the whole universe.'
+			'Yes. Every single time we launch a new product or service, there will be associated benefits for Emerald Pass holders. This makes holding the Pass not only beneficial right now, but for when we do special giveaways, beta tests, and introduce new features on other projects.'
 	}
 ];
 
