8668bdb5693b1b8666844d0b1fee95d9d8cbcd44
jacob-tucker <jacobtucker818@gmail.com>
2022-09-20 01:15:29 -0500

    update descriptions

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -3,15 +3,15 @@ const projects = [
 		name: 'Emerald Academy',
 		slogan: 'Learn everything about the Flow Blockchain and Cadence',
 		url: 'https://academy.ecdao.org/',
-		logo: '/ec-logo.png',
-		benefits: ['Receive discounts on paid courses/bootcamps', 'Access to Achievements page']
+		logo: '/academy-logo.png',
+		benefits: ['Receive discounts on paid courses/bootcamps', 'Access to automatic resume builder (not yet completed)']
 	},
 	{
 		name: 'FLOAT',
 		slogan: 'Proof of Attendance Token',
 		url: 'https://floats.city/',
 		logo: '/projects-logos/float-logo.png',
-		benefits: ['Access to Challenges feature']
+		benefits: ['Access to creating Challenges']
 	},
 	{
 		name: 'Touchstone',
@@ -19,9 +19,10 @@ const projects = [
 		url: 'https://touchstone.city/',
 		logo: '/projects-logos/touchstone-logo.png',
 		benefits: [
+			'Airdrop to a batch of users',
 			'Your own custom URL',
 			'Add to NFT Catalog with 1 click',
-			'Airdrop to a batch of users'
+			'Access extra stats and list of users who claim NFTs'
 		]
 	},
 	{
